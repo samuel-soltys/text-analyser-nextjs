63269f017e7fbcd96bdffb1ecbd6f0c236942240
Samuel Šoltys <samuelsasanka@gmail.com>
2023-01-04 23:06:21 +0100

    Textarea, Word count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-    return (<></>);
+    const [text, setText] = useState("");
+
+    function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        setText(e.target.value);
+    }
+
+    // Words
+    function countWords(str: String) {
+        var regex = /[\p{L}0-9-']+/gu;
+        var words = str.match(regex);
+        return words ? words.length : 0;
+    }
+    
+    return (
+        <section>
+            <textarea className="text-prompt" value={text} onChange={handleChange} />
+            <br />
+            <br />
+            <p>Words: {countWords(text)}</p>
+        </section>
+    );
 };
 
 export default Home;
