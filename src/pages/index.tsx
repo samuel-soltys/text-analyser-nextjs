6c0035a7b9cb6cc396f3ef0cbb578425886e5a8c
Samuel Šoltys <samuelsasanka@gmail.com>
2023-01-04 23:12:37 +0100

    Syllable count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,50 @@ const Home: NextPage = () => {
         var sentences = str.match(regex);
         return sentences ? sentences.length : 0;
     }
-    
+
+    // Syllables
+    function countSyllablesWord(word: String) {
+        word = word.toLowerCase();
+        if (word.length <= 3) {
+            return 1;
+        }
+        word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, "");
+        word = word.replace(/^y/, "");
+        if (word.match(/[aeiouy]{1,2}/g)) {
+            return word.match(/[aeiouy]{1,2}/g)!.length;
+        } else {
+            return 0;
+        }
+    }
+    function countAllSyllables(str: String) {
+        var words = separateToWords(str);
+        var syllables = 0;
+        if (words) {
+            for (var i = 0; i < words.length; i++) {
+                syllables += countSyllablesWord(words[i]);
+            }
+        } else {
+            return 0;
+        }
+        return syllables;
+    }
+
+    // Complex words
+    function countComplexWords(str: String) {
+        var words = separateToWords(str);
+        var complex = 0;
+        if (words) {
+            for (var i = 0; i < words.length; i++) {
+                if (countSyllablesWord(words[i]) >= 3) {
+                    complex++;
+                }
+            }
+        } else {
+            return 0;
+        }
+        return complex;
+    }
+
     return (
         <section>
             <textarea className="text-prompt" value={text} onChange={handleChange} />
@@ -45,6 +88,8 @@ const Home: NextPage = () => {
             <p>Characters with spaces: {countCharacters(text)}</p>
             <p>Characters without spaces: {countCharactersNoSpace(text)}</p>
             <p>Sentences: {countSentences(text)}</p>
+            <p>Complex Words (3 or more syllables): {countComplexWords(text)}</p>
+            <p>Syllables (English): {countAllSyllables(text)}</p>
         </section>
     );
 };
